9d92fd749e9ec0750a43a712057397fadcf95032
Ed Summers <ehs@pobox.com>
2010-02-01 15:02:31 -0500

    moved to nose for tests

--- setup.py
+++ setup.py
@@ -11,16 +11,6 @@ Programming Language :: Python
 Topic :: Text Processing :: General
 """
 
-# workaround for older pythons that don't 
-# understand classifiers 
-
-if sys.version_info < (2, 3):
-    _setup = setup
-    def setup(**kwargs):
-        if kwargs.has_key("classifiers"):
-            del kwargs["classifiers"]
-        _setup(**kwargs)
-
 setup( 
     name             = 'opensearch',
     version          = '0.8',
@@ -28,8 +18,10 @@ setup(
     author           = 'Ed Summers',
     author_email     = 'ehs@pobox.com',
     license          = 'http://www.opensource.org/licenses/gpl-license.php',
-    packages         = find_packages(),
+    packages         = find_packages(exclude=['test']),
     description      = "Interact with opensearch services",
     classifiers      = filter( None, classifiers.split("\n") ),
+    tests_require    = ["nose>=0.10.4"],
+    test_suite       = 'nose.collector',
 )
 
