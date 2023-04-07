e25e5c050682ca76cd51d88230be8cf36efd5218
Christian Clauss <cclauss@me.com>
2023-04-07 11:33:51 +0200

    Update __init__.py

--- opensearch/__init__.py
+++ opensearch/__init__.py
@@ -1,4 +1,4 @@
-from .description import Description
-from .query import Query, Param
-from .client import Client
-from .results import Results
+from opensearch.description import Description
+from opensearch.query import Query, Param
+from opensearch.client import Client
+from opensearch.results import Results
